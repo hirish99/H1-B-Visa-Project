7343dff86bb875525432b9fefbeebb4a1c47050d
hirish99 <32660271+hirish99@users.noreply.github.com>
2018-01-20 02:26:10 -0800

    Update ML.py

--- src/ML.py
+++ src/ML.py
@@ -6,3 +6,10 @@ h1b_data = returnClean()
 X = h1b_data.drop('CASE_STATUS', axis=1)
 Y = h1b_data['CASE_STATUS']
 
+import sklearn
+from sklearn.svm import LinearSVC
+
+clf = LinearSVC()
+clf.fit(X, Y)
+
+clf.predict(X)
