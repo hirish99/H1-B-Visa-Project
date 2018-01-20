074586432d3b4b7655ec9a494d49c630a77f541c
hirish99 <32660271+hirish99@users.noreply.github.com>
2018-01-20 02:45:52 -0800

    Add files via upload

--- src/ML.py
+++ src/ML.py
@@ -3,6 +3,11 @@ import numpy as np
 from clean import returnClean
 
 h1b_data = returnClean()
+
+#Train/Test Split:
+train = h1b_data.sample(frac=0.8, random_state=200)
+test = h1b_data.drop(train.index)
+
 X = h1b_data.drop('CASE_STATUS', axis=1)
 Y = h1b_data['CASE_STATUS']
 
@@ -12,4 +17,7 @@ from sklearn.svm import LinearSVC
 clf = LinearSVC()
 clf.fit(X, Y)
 
-clf.predict(X)
+result = test
+result['OUTPUT'] = clf.predict(test.drop('CASE_STATUS', axis=1))
+result.drop(['EMPLOYER_NAME', 'SOC_NAME', 'JOB_TITLE', 'FULL_TIME_POSITION', 'PREVAILING_WAGE', 'YEAR', 'WORKSITE'], axis=1, inplace=True)
+result.to_csv('OUTPUT.CSV')
