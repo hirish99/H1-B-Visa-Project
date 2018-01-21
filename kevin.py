b0ec9275132b5376c46c3d2e4bfb2d22bba12304
hirish99 <32660271+hirish99@users.noreply.github.com>
2018-01-21 02:38:08 -0800

    Add files via upload

--- kevin.py
+++ kevin.py
@@ -4,15 +4,14 @@ from clean import returnClean
 
 def kevin(n, SOC_NAME, FULL_TIME_POSITION, PREVAILING_WAGE, WORKSITE, lon, lat):
 
-    h1b_data = returnClean(n)
+    h1b_data = returnClean(n, n)
 
     #Train/Test Split:
     train = h1b_data
-
+    
     #Dropping JobTitle
     X = train.drop(['CASE_STATUS','JOB_TITLE'], axis=1)
     Y = train['CASE_STATUS']
-
 
     import sklearn
     from sklearn.neighbors import KNeighborsClassifier
